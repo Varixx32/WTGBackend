a1ace7cc42b4a4d214bb5742d4a6303fa990d778
Victor Tercero <victor.tercerom@viewnext.com>
2025-05-13 18:28:01 +0200

    Starter project

---
+++ src/main/java/com/victortercero/whattogift/WhatToGiftApiApplication.java
@@ -0,0 +1,13 @@
+ package com.victortercero.whattogift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToGiftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToGiftApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victortercero/whattogift/WhatToGiftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victortercero.whattogift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToGiftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

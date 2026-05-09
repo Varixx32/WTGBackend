e272b00e715a6f69b74f08c01df91751921cf46e
Victor Tercero <55060772+Varixx32@users.noreply.github.com>
2026-05-09 19:18:38 +0200

    Proyecto creado con Spring Initializr

---
+++ src/main/java/com/victortercero/whattogift/WhattogiftApplication.java
@@ -0,0 +1,13 @@
+package com.victortercero.whattogift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattogiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhattogiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victortercero/whattogift/WhattogiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victortercero.whattogift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattogiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
